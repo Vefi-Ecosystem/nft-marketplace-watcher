1cd0f18e0a49cf2d9dc29896e280b4af62481364
Kingsley Victor <javaprodigy56@gmail.com>
2022-05-05 02:54:00 +0100

    create order model

---
+++ src/db/models/order.ts
@@ -0,0 +1,34 @@
+import { Sequelize, DataTypes, Model, ModelStatic } from 'sequelize';
+
+export default class Order {
+  model: ModelStatic<Model<any, any>>;
+
+  constructor(sequelize: Sequelize) {
+    this.model = sequelize.define('Order', {
+      orderId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true
+      },
+      to: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      tokenId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      bidCurrency: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      amount: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      collection: {
+        type: DataTypes.STRING
+      }
+    });
+  }
+}

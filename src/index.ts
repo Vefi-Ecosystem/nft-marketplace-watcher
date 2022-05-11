4e7fc3219a34a35672ab8826acd28289df748f17
Kingsley Victor <javaprodigy56@gmail.com>
2022-05-11 13:25:28 +0100

    made updates

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { sequelize } from './db';
 import { handleEvents } from './watcher';
 import chains from './chains';
 import router from './api/router';
+import logger from './logger';
 
 const port: number = parseInt(process.env.PORT || '6008');
 const app: express.Express = express();
@@ -19,15 +20,15 @@ app.use((req, res, next) => {
 app.use('/api', router);
 
 app.listen(port, () => {
-  console.log('App is running on: %d', port);
+  logger('App is running on: %d', port);
 
   (() => {
     sequelize.sync().then(() => {
-      console.log('Sequelize connected to DB');
+      logger('Sequelize connected to DB');
 
       for (const network of chains) {
         handleEvents(<string>network.url, <string>network.contractAddress, network.name);
-        console.log('Now watching smart contract on: %s', network.name);
+        logger('Now watching smart contract on: %s', network.name);
       }
     });
   })();

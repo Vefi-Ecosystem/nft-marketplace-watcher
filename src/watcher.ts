a8cc19394a1d56572ff3fef9803c83153914667e
Kingsley Victor <javaprodigy56@gmail.com>
2022-06-11 09:41:07 +0100

    ...

--- src/watcher.ts
+++ src/watcher.ts
@@ -11,6 +11,7 @@ import {
   handleOrderCancelledEvent,
   handleOrderRejectedEvent
 } from './handlers';
+import { Networkish } from '@ethersproject/providers';
 
 const collectionDeployedHash: string = hashId('CollectionDeployed(address,address,uint256,string,string,string)');
 const mintHash: string = hashId('Mint(address,uint256,uint256,string,address)');
@@ -24,15 +25,15 @@ const orderCancelledHash: string = hashId('OrderItemCancelled(bytes32,uint256)')
 const orderEndedHash: string = hashId('OrderItemEnded(bytes32,uint256)');
 const orderRejectedHash: string = hashId('OrderItemRejected(bytes32, uint256)');
 
-export function handleEvents(url: string, address: string, network: string) {
-  const provider = buildProvider(url, undefined);
+export function handleEvents(url: string, address: string, network: string, networkish?: Networkish) {
+  const provider = buildProvider(url, networkish);
 
-  provider.on({ address, topics: [collectionDeployedHash] }, handleCollectionDeploymentEvent(network, url));
+  provider.on({ address, topics: [collectionDeployedHash] }, handleCollectionDeploymentEvent(network, url, networkish));
   provider.on({ address, topics: [mintHash] }, handleMintEvent(network));
-  provider.on({ address, topics: [marketItemCreatedHash] }, handleMarketItemCreatedEvent(url, network));
+  provider.on({ address, topics: [marketItemCreatedHash] }, handleMarketItemCreatedEvent(url, network, networkish));
   provider.on({ address, topics: [marketItemCancelledHash] }, handleMarketItemCancelledEvent(network));
-  provider.on({ address, topics: [saleMadeHash] }, handleSaleMadeEvent(url, network));
-  provider.on({ address, topics: [orderMadeHash] }, handleOrderMadeEvent(url, network));
+  provider.on({ address, topics: [saleMadeHash] }, handleSaleMadeEvent(url, network, networkish));
+  provider.on({ address, topics: [orderMadeHash] }, handleOrderMadeEvent(url, network, networkish));
   provider.on({ address, topics: [orderCancelledHash] }, handleOrderCancelledEvent(network));
   provider.on({ address, topics: [orderEndedHash] }, handleOrderEndedEvent(network));
   provider.on({ address, topics: [orderRejectedHash] }, handleOrderRejectedEvent(network));
